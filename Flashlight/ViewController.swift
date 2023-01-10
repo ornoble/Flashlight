035f6a94499773d1d8a211447c35b7e8c448b6dc
Oliver Lee <ntxsunset06@gmail.com>
2023-01-10 10:41:34 -0700

    Initial Commit

---
+++ Flashlight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by Lee, Oliver - Student on 1/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
